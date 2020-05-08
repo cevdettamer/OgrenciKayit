8d534e7311cbfd6139b2efd013a772234b39b4da
Tamer Guven <ctamerguven@gmail.com>
2020-05-08 16:30:00 +0300

    first

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OgrenciKayit.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Hakkımızda Sayfası.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "İletişim Sayfası.";
+
+            return View();
+        }
+    }
+}
